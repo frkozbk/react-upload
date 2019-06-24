81a26ecc8ac136e8710c41dd3e7cdf6f50f744ac
Faruk@12 <ozbekfaruk12@gmail.com>
2019-06-24 17:06:58 +0300

    initial

---
+++ client/src/components/Message.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+const Message = ({msg}) => {
+    return (
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+  {msg}
+  <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+    )
+}
+
+export default Message
+
+Message.propTypes = {
+    msg:PropTypes.string.isRequired,
+
+}
---
+++ client/src/components/Progress.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Progress = ({percentage}) => {
+    return (
+        <div class="progress">
+  <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style={{width:`${percentage}%`}} >{percentage}%</div>
+  
+</div>
+    )
+}
+
+Progress.propTypes = {
+    percentage:PropTypes.number.isRequired,
+}
+
+export default Progress
